5f098450a4b59711a964f9e6d7fdec92c052f6e5
Joseph Kim <joseph.kim.sungha@gmail.com>
2018-03-14 11:45:43 +1100

    Changed implementation and made user interface look nice

--- index.js
+++ index.js
@@ -6,41 +6,33 @@ app.get("/", function(req, res){
   res.sendFile(__dirname + "/index.html");
 });
 
-const startGame = function(player1, player2) {
-  const gameBoard = new Array(9);
-  let gameStatus = "playing";
-
-  const playMove = function(player, moveId) {
-    gameBoard[moveId] = player === "p1" ? "X" : "O";
-    player1.emit("move", gameBoard);
-    player2.emit("move", gameBoard);
-  }
-
-  player1.on("move", moveId => playMove("p1", moveId));
-  player2.on("move", moveId => playMove("p2", moveId));
-}
-
-const queue = [];
+let roomNumber = 0;
+const roomsReference = {};
+const gameBoards = {};
 
 io.on("connection", socket => {
-  if (queue.length == 0) {
-    queue.push(socket);
-    console.log(socket.id, "joined the queue. Current queue:", queue.map(s => s.id));
-  } else {
-    const player1 = socket;
-    const player2 = queue[0];
-    queue.splice(0, 1);
-    console.log(player1.id, "and", player2.id, "have joined a room. New queue:", queue.map(s => s.id));
-    startGame(player1, player2);
-  }
+  if (io.sockets.adapter.rooms[roomNumber] && io.sockets.adapter.rooms[roomNumber].length > 1) roomNumber++;
+  socket.join(roomNumber, () => {
+    roomsReference[socket.id] = roomNumber;
+    console.log(`${socket.id} joined room #${roomNumber}`);
 
-  socket.on("disconnect", () => {
-    const index = queue.map(s => s.id).indexOf(socket.id);
-    if (index > -1) {
-      queue.splice(index, 1);
+    if (!gameBoards[roomNumber]) {
+      gameBoards[roomNumber] = new Array(9);
+      console.log(`Gameboard was made for room #${roomNumber}`);
     }
-    console.log(socket.id, "disconnected. New queue:", queue.map(s => s.id));
-  })
+
+    if (io.sockets.adapter.rooms[roomsReference[socket.id]].length > 1) {
+      io.to(roomsReference[socket.id]).emit("start");
+    }
+
+    socket.on("move", moveId => {
+      // check validity of move
+
+      io.to(roomsReference[socket.id]).emit("move", moveId, socket.id);
+      
+      // check if the move was a winning move
+    });
+  });
 })
 
 http.listen(3000, function(){
