af91bed1420a9ea35157b69183700971306559d3
Joseph Kim <joseph.kim.sungha@gmail.com>
2018-03-14 15:41:31 +1100

    Queue system implemented to allow multiple instances of the game

--- index.js
+++ index.js
@@ -1,22 +1,45 @@
 const app = require("express")();
 const http = require("http").Server(app);
-var io = require('socket.io')(http);
+const io = require("socket.io")(http);
 
 app.get("/", function(req, res){
   res.sendFile(__dirname + "/index.html");
 });
 
+const startGame = function(player1, player2) {
+  const gameBoard = new Array(9);
+  let gameStatus = "playing";
+
+  const playMove = function(player, moveId) {
+    gameBoard[moveId] = player === "p1" ? "X" : "O";
+    player1.emit("move", gameBoard);
+    player2.emit("move", gameBoard);
+  }
+
+  player1.on("move", moveId => playMove("p1", moveId));
+  player2.on("move", moveId => playMove("p2", moveId));
+}
+
+const queue = [];
+
 io.on("connection", socket => {
-  console.log("A user connected with id ", socket.id);
-  socket.join("the only room", () => {
-    socket.on("move", (moveId) => {
-      console.log(moveId);
-      io.sockets.to("the only room").emit("move", moveId);
-    })
-  })
+  if (queue.length == 0) {
+    queue.push(socket);
+    console.log(socket.id, "joined the queue. Current queue:", queue.map(s => s.id));
+  } else {
+    const player1 = socket;
+    const player2 = queue[0];
+    queue.splice(0, 1);
+    console.log(player1.id, "and", player2.id, "have joined a room. New queue:", queue.map(s => s.id));
+    startGame(player1, player2);
+  }
 
   socket.on("disconnect", () => {
-    console.log("Disconnect");
+    const index = queue.map(s => s.id).indexOf(socket.id);
+    if (index > -1) {
+      queue.splice(index, 1);
+    }
+    console.log(socket.id, "disconnected. New queue:", queue.map(s => s.id));
   })
 })
 
