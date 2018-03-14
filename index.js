63e392b26585f9c877ec2780bdb829834300baf9
Joseph Kim <joseph.kim.sungha@gmail.com>
2018-03-14 14:17:19 +1100

    Multiple users can join a room and send their moves to the server

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 const app = require("express")();
 const http = require("http").Server(app);
+var io = require('socket.io')(http);
 
 app.get("/", function(req, res){
   res.sendFile(__dirname + "/index.html");
 });
 
+io.on("connection", socket => {
+  console.log("A user connected with id ", socket.id);
+  socket.join("the only room", () => {
+    socket.on("move", (moveId) => {
+      console.log(moveId);
+      io.sockets.to("the only room").emit("move", moveId);
+    })
+  })
+
+  socket.on("disconnect", () => {
+    console.log("Disconnect");
+  })
+})
+
 http.listen(3000, function(){
-  console.log('listening on *:3000');
+  console.log("Your game is live at localhost:3000");
 });
