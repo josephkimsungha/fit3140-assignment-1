20ea0fe21473dec767c7ace334cac2a2cc949e9b
Joseph Kim <joseph.kim.sungha@gmail.com>
2018-03-17 15:34:28 +1100

    Completed a huge amount of the game oops

--- index.js
+++ index.js
@@ -2,6 +2,41 @@ const app = require("express")();
 const http = require("http").Server(app);
 const io = require("socket.io")(http);
 
+function checkBoard(board, playerNumber) {
+  // check rows
+  for (let i = 0; i < 9; i += 3) {
+    const row = board.slice(i, i + 3);
+    if (!row.includes(undefined) && row.every(number => number == playerNumber)) {
+      return "win";
+    }
+  }
+
+  // check columns
+  for (let i = 0; i < 3; i++) {
+    const column = board.filter((number, index) => index % 3 == i);
+    if (column.length == 3 && column.every(number => number == playerNumber)) {
+      return "win";
+    }
+  }
+
+  // check diagonals
+  const mainDiagonal = board.filter((number, index) => [0, 4, 8].indexOf(index) > -1);
+  if (mainDiagonal.length == 3 && mainDiagonal.every(number => number == playerNumber)) {
+    return "win";
+  }
+
+  const secondaryDiagonal = board.filter((number, index) => [2, 4, 6].indexOf(index) > -1);
+  if (secondaryDiagonal.length == 3 && secondaryDiagonal.every(number => number == playerNumber)) {
+    return "win";
+  }
+
+  if (!board.includes(undefined)) {
+    return "draw";
+  }
+
+  return;
+}
+
 app.get("/", function(req, res){
   res.sendFile(__dirname + "/index.html");
 });
@@ -18,20 +53,56 @@ io.on("connection", socket => {
     console.log(`${socket.id} joined room #${roomNumber} as player ${playerNumber}`); // When player 1 leaves, player 2 needs to be set to player 1 somehow
 
     if (!gameBoards[roomNumber]) {
-      gameBoards[roomNumber] = new Array(9);
+      gameBoards[roomNumber] = { board: new Array(9), playerTurn: 1, status: "waiting" };
       console.log(`Gameboard was made for room #${roomNumber}`);
     }
 
     if (io.sockets.adapter.rooms[roomsReference[socket.id].roomNumber].length > 1) {
       io.to(roomsReference[socket.id].roomNumber).emit("start");
+      socket.to(roomsReference[socket.id].roomNumber).emit("newTurn");
+      gameBoards[roomsReference[socket.id].roomNumber].status = "playing";
     }
 
     socket.on("move", moveId => {
-      // check validity of move
+      const { roomNumber, playerNumber } = roomsReference[socket.id];
+      const { board, playerTurn, status } = gameBoards[roomNumber];
 
-      io.to(roomsReference[socket.id].roomNumber).emit("move", moveId, roomsReference[socket.id].playerNumber);
+      if (status != "playing") {
+        io.to(socket.id).emit("notPlaying");
+      } else {
+        // check player turn
+        if (playerNumber != playerTurn) {
+          io.to(socket.id).emit("wrongTurn");
+        } else {
+          // check validity of move
+          if (board[moveId]) {
+            io.to(socket.id).emit("alreadyPlayed");
+          } else {
+            // update board
+            board[moveId] = playerNumber;
 
-      // check if the move was a winning move
+            // send move out to players
+            io.to(roomsReference[socket.id].roomNumber).emit("move", moveId, roomsReference[socket.id].playerNumber);
+
+            // check if the move was a winning move or game has ended in a tie
+            switch (checkBoard(board, playerNumber)) {
+              case "win":
+              gameBoards[roomNumber].status = "finished";
+              console.log("win!");
+              break;
+              case "draw":
+              gameBoards[roomNumber].status = "finished";
+              console.log("draw");
+              break;
+              default:
+              console.log("changing turn");
+              // change turn
+              gameBoards[roomNumber].playerTurn = 1 + playerTurn % 2;
+              socket.to(roomNumber).emit("newTurn");
+            }
+          }
+        }
+      }
     });
   });
 })
